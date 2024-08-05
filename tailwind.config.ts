d4f56dc449b26b6060032bb789bf0f1837244f02
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-08-05 14:37:24 -0300

    build: configuraçoes tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,20 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
   plugins: [],
+  theme: {
+    colors: {
+      black: {
+        800: '#2f2f2f'
+      },
+      gray : {
+        400: '#B8B8B8',
+        500:'#626262',
+      },
+      teal: '#1dbba5',
+      white: '#ffffff',
+    }
+  },
 };
+
 export default config;
