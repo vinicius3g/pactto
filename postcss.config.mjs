2ee78043ead14a3cf3315c8869e4de25b3e177b8
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-08-05 15:40:11 -0300

    build: ajustes nas configuraçoes do projeto

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    autoprefixer: {},
+    'postcss-import': {},
     tailwindcss: {},
+    'tailwindcss/nesting': {},
   },
 };
 
