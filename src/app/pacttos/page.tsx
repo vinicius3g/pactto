44984413dc0d8afcbf0418ab55a80519280a7326
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-08-08 19:05:33 -0300

    feat: page asjuste

--- src/app/pacttos/page.tsx
+++ src/app/pacttos/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from 'next';
 import { Pacttos } from '.'
 import '@/../styles/index.css';
+
+export const metadata: Metadata = {
+  description: 'pacttos',
+  title: 'pacttos',
+}
 
 export default function Page() {
   return <Pacttos />
