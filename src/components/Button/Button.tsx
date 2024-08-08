243b9fbc048a970f44a00a658942cfebb0c9fcc4
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-08-08 13:17:26 -0300

    feat(Button): implemetaçao de novos parametros

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,12 +5,15 @@ import styles from './Button.module.css'
 interface IButton {
   text: string;
   icon?: boolean;
+  color?: 'primary' | 'secondary';
   handleExec: () => void;
 }
 
-const Button: React.FC<IButton> = ({ text, icon, handleExec }) => {
+const Button: React.FC<IButton> = ({ text, icon, color = 'primary', handleExec }) => {
+  const colorSelect = color === 'primary' ? styles.primary : styles.secondary
+
   return (
-    <button className={styles.button} onClick={handleExec}>
+    <button className={`${styles.button} ${colorSelect}`} onClick={handleExec}>
       {icon && <AddIcon sx={{ color: '#232225', fontSize: 18, marginRight: 1 }} />}
       <span className='font-medium uppercase'>{text}</span>
     </button>
