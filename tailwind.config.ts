3cc9da4bdfbd9da2f14e73a94f3ac68a5a6a6d21
vinicius.paixao <vinicius.ds.paixao@gmail.com>
2024-08-13 14:21:47 -0300

    chore(prettier): add prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   plugins: [],
   theme: {
@@ -14,18 +14,18 @@ const config: Config = {
         800: '#212121',
         900: '#181818'
       },
-      gray : {
+      gray: {
         300: '#cbcbcb',
         400: '#B8B8B8',
-        500:'#626262',
+        500: '#626262',
         700: '#545454',
         600: '#2f2f2f',
         800: '#3a363f'
       },
       teal: '#1dbba5',
-      white: '#ffffff',
+      white: '#ffffff'
     }
-  },
+  }
 };
 
 export default config;
